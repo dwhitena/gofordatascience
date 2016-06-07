e5ca9e3ed627a666889468f45bc2994a10cd0128
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-06-07 15:19:40 -0500

    repofilter image and pipeline

--- golang-builder-latest/build.sh
+++ golang-builder-latest/build.sh
@@ -4,8 +4,13 @@ source /build_environment.sh
 
 # time the compile of the statically linked version of package
 START_TIME=$(date +%s%N)
-go install $pkgName/...
+go install $pkgName/... > compile.log
 ELAPSED_TIME=$(($(date +%s%N) - $START_TIME))
 
-echo $ELAPSED_TIME
-#time go install -v $pkgName/...
+lines=`wc -l compile.log | awk '{print $1}'`;
+if [ $lines -eq 0 ];
+then
+   echo $REPONAME, $ELAPSED_TIME
+else
+   echo $REPONAME, "error"
+fi
---
+++ repofilter/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/csv"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+
+	csvfile, err := os.Open("/pfs/repodata.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvfile.Close()
+
+	reader := csv.NewReader(csvfile)
+	reader.FieldsPerRecord = -1
+	rawCSVdata, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, each := range rawCSVdata {
+		d1 := []byte(each[0])
+		err = ioutil.WriteFile("/pfs/out/"+each[0], d1, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
